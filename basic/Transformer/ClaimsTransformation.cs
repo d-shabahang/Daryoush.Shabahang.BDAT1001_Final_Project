8ed581190e4b8d19eb0a26732727cf39192b63dd
Daryoush Shabahang <347749+d-shabahang@users.noreply.github.com>
2021-08-17 21:51:16 -0400

    Add files via upload

---
+++ basic/Transformer/ClaimsTransformation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics_Final_Project_InformationEncoding.Transformer
+{
+    public class ClaimsTransformation : IClaimsTransformation
+    {
+        // every time the user is authenticated, this will be used multiple times
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            var hasFriendClaim = principal.Claims.Any(x => x.Type == "Friend");
+
+            if (!hasFriendClaim)
+            {
+                ((ClaimsIdentity)principal.Identity).AddClaim(new Claim("Friend", "Bad"));
+            }
+
+            return Task.FromResult(principal);
+        }
+    }
+}
