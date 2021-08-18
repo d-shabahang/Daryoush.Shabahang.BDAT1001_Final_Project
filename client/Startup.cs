93f24eb1d0ab9127dead4271106920eb0a96b703
Daryoush Shabahang <347749+d-shabahang@users.noreply.github.com>
2021-08-17 21:52:55 -0400

    Add files via upload

---
+++ client/Startup.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(config =>
+            {
+                // all we are doing is to check the cookie to confirm that we are authenticated
+                config.DefaultAuthenticateScheme = "ClientCookie";
+                // we are basically saying that  when we sign in, we will provide a cookie
+                config.DefaultSignInScheme = "ClientCookie";
+                // we will use this to check if we are allowed to do something
+                config.DefaultChallengeScheme = "OurServer";
+            })
+                .AddCookie("ClientCookie")
+                .AddOAuth("OurServer", config =>
+                {
+                    config.ClientId = "client_id";
+                    config.ClientSecret = "client_secret";
+                    config.CallbackPath = "/oauth/callback";
+                    config.AuthorizationEndpoint = "https://localhost:44307/oauth/authorize";
+                    config.TokenEndpoint = "https://localhost:44307/oauth/token";
+                    config.SaveTokens = true; // save tokens in our cookie
+
+                    config.Events = new OAuthEvents()
+                    {
+                        OnCreatingTicket = context =>
+                        {
+                            var accessToken = context.AccessToken;
+                            var base64payload = accessToken.Split('.')[1];
+                            var bytes = Convert.FromBase64String(base64payload);
+                            var jsonPayload = Encoding.UTF8.GetString(bytes);
+                            var claims = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonPayload);
+
+                            foreach (var claim in claims)
+                            {
+                                context.Identity.AddClaim(new Claim(claim.Key, claim.Value));
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddHttpClient();
+
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // who are you?
+            app.UseAuthentication();
+
+            // are you allowed?
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
